5af9421925b8ce4ec99c32e6446111f5e87e95c2
Raro <etobro@gmail.com>
2023-12-06 16:35:24 +0100

    code format and add variables

--- vault_server/iam.tf
+++ vault_server/iam.tf
@@ -1,41 +1,41 @@
 #creating IAM Instance Profiile
 data "aws_iam_policy_document" "assume_role" {
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["sts:AssumRole"]
     principals {
-      type = "service"
-      identifiers = [ "ec2.amazonaws.com" ]
+      type        = "service"
+      identifiers = ["ec2.amazonaws.com"]
     }
   }
 }
 
 data "aws_iam_policy_document" "vault-kms-unseal" {
-    statement {
-      sid = "VaultKMSUnseal"
-      effect = "Allow"
-      resources = [ aws_kms_key.vault.arn ]
-      actions = [ 
-        "kms:Encrypt",
-        "kms:Decrypt",
-        "kms:Descriibekey",
-       ]
-    }
-  
+  statement {
+    sid       = "VaultKMSUnseal"
+    effect    = "Allow"
+    resources = [aws_kms_key.vault.arn]
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:Descriibekey",
+    ]
+  }
+
 }
 
 resource "aws_iam_role" "vault-kms-unseal" {
-    name = "vault-kms-role"
-    assume_role_policy = data.aws_iam_policy_document.assume_role.json  
+  name               = "vault-kms-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy" "vault-kms-unseal" {
-    name = "vault-kms-unseal"
-    role = aws_iam_role.vault-kms-unseal.id
-    policy = data.aws_iam_policy_document.vault-kms-role.json
+  name   = "vault-kms-unseal"
+  role   = aws_iam_role.vault-kms-unseal.id
+  policy = data.aws_iam_policy_document.vault-kms-role.json
 }
 
 resource "aws_iam_instance_profile" "vault-kms-unseal" {
-    name = "vault-kms-unseal"
-    role = aws_iam_role.vault-kms-unseal.name
+  name = "vault-kms-unseal"
+  role = aws_iam_role.vault-kms-unseal.name
 }
--- vault_server/vault.tfvars
+++ vault_server/vault.tfvars
@@ -2,10 +2,10 @@ profilename = raroaccess
 
 aws-region = us-east-1
 
-vault-ami
+vault-ami = ami-0fc5d935ebf8bc3bc
 
-instance_type
+instance_type = t2.micro
 
-domain-name
+domain-name = 
 
-email
+email =
